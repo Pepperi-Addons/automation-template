778c6b9b195435f5cfa06c6caf0a17f30442db55
Lior Sela <lior.s@pepperi.com>
2023-01-31 10:35:37 +0200

    cancel commented tests

--- server-side/tests/api_tests/SyncTests.test.ts
+++ server-side/tests/api_tests/SyncTests.test.ts
@@ -25,54 +25,55 @@ export async function SyncTests(generalService: GeneralService, addonService: Ge
         const papiClient = addonService.papiClient; 
         let dateTime = new Date()
 
-        // it('baseSyncTest', async () => {
-        //     // initializing adal schema with data, first property is number of fields
-        //     // second propety is number of characters in each field
-        //     let adalTable = await syncTestService.initAdalTable(1,1)
-        //     // sleeping for the changes to apply and sync wont be up to date
-        //     generalService.sleep(3000)
-        //     dateTime.setHours(dateTime.getHours()-1)
-        //     // calling sync api endpoint
-        //     let syncRes = await syncTestService.callSyncPullAPI(dateTime.toISOString())
-        //     // expecting to have up to date which is false due to the record that was inserted
-        //     //  expecting execution uri - has the data of the sync action
-        //     expect(syncRes).to.have.property('UpToDate').that.is.a('Boolean').and.is.equal(false)
-        //     expect(syncRes).to.have.property('ExecutionURI').that.is.a('String').and.is.not.undefined
-        //     // getting result from audit log - From Execution URI
-        //     let auditLog = await syncTestService.getAuditLogData(syncRes)
-        //     // using execution uri getting the data that was updated in sync
-        //     let schemes = await syncTestService.getSchemesFromAudit(auditLog)
-        //     expect(schemes).to.contain(syncTestService.scehmaName)
-        // })
-        // it('futureDate',async()=>{
+        it('baseSyncTest', async () => {
+            // initializing adal schema with data, first property is number of fields
+            // second propety is number of characters in each field
+            let adalTable = await syncTestService.initAdalTable(1,1)
+            // sleeping for the changes to apply and sync wont be up to date
+            generalService.sleep(3000)
+            dateTime.setHours(dateTime.getHours()-1)
+            // calling sync api endpoint
+            let syncRes = await syncTestService.callSyncPullAPI(dateTime.toISOString())
+            // expecting to have up to date which is false due to the record that was inserted
+            //  expecting execution uri - has the data of the sync action
+            expect(syncRes).to.have.property('UpToDate').that.is.a('Boolean').and.is.equal(false)
+            expect(syncRes).to.have.property('ExecutionURI').that.is.a('String').and.is.not.undefined
+            // getting result from audit log - From Execution URI
+            let auditLog = await syncTestService.getAuditLogData(syncRes)
+            // using execution uri getting the data that was updated in sync
+            let schemes = await syncTestService.getSchemesFromAudit(auditLog)
+            expect(schemes).to.contain(syncTestService.scehmaName)
+        })
+        it('futureDate',async()=>{
             
-        //     // adding 10 years to current date for future date validation
-        //     dateTime.setFullYear(dateTime.getFullYear()+10)
-        //     // checking performance - check time before and after sync request
-        //     const t1 = performance.now()
-        //     let res =  await syncTestService.callSyncPullAPI(dateTime.toISOString())
-        //     const t2 = performance.now()
-        //     // expecting the future date to take less than 5s
-        //     expect(t2-t1).to.be.a('Number').and.to.be.lessThanOrEqual(5000)
-        //     // expecting up to date will be true - there are no records with future time
-        //     expect(res).to.have.property('UpToDate').that.is.a('Boolean').and.is.equal(true)
-        // })
+            // adding 10 years to current date for future date validation
+            dateTime.setFullYear(dateTime.getFullYear()+10)
+            // checking performance - check time before and after sync request
+            const t1 = performance.now()
+            let res =  await syncTestService.callSyncPullAPI(dateTime.toISOString())
+            const t2 = performance.now()
+            // expecting the future date to take less than 5s
+            expect(t2-t1).to.be.a('Number').and.to.be.lessThanOrEqual(5000)
+            // expecting up to date will be true - there are no records with future time
+            expect(res).to.have.property('UpToDate').that.is.a('Boolean').and.is.equal(true)
+        })
 
-        // it('returnURLTest', async () => {
-        //     // creating scheme and record in adal
-        //     let adalTable = await syncTestService.initAdalTable(1,1)
-        //     generalService.sleep(500)
-        //     // resetting date and changing to previous hour
-        //     dateTime = new Date()
-        //     dateTime.setHours(dateTime.getHours()-1)
-        //     // calling sync pull endpoint and requesting to have a file of changes
-        //     let auditLog = await syncTestService.callReturnUrlAPI(dateTime.toISOString())
-        //     // validating that the file url is returned
-        //     expect(auditLog).to.have.property('ResourcesURL').that.is.a('String').and.is.not.undefined
-        //     let schemes = await syncTestService.getSchemesFromUrl(auditLog)
-        //     // validating that the created schema in the file that was received 
-        //     expect(schemes).to.contain(syncTestService.scehmaName)
-        // })
+        it('returnURLTest', async () => {
+            // creating scheme and record in adal
+            let adalTable = await syncTestService.initAdalTable(1,1)
+            generalService.sleep(500)
+            // resetting date and changing to previous hour
+            dateTime = new Date()
+            dateTime.setHours(dateTime.getHours()-1)
+            // calling sync pull endpoint and requesting to have a file of changes
+            let auditLog = await syncTestService.callReturnUrlAPI(dateTime.toISOString())
+            // validating that the file url is returned
+            expect(auditLog).to.have.property('ResourcesURL').that.is.a('String').and.is.not.undefined
+            let schemes = await syncTestService.getSchemesFromUrl(auditLog)
+            // validating that the created schema in the file that was received 
+            expect(schemes).to.contain(syncTestService.scehmaName)
+        })
+
         it('SystemFilter Type=None',async()=>{
             let accounts = await syncTestService.initAdalAccountRefTable()
             let users = await syncTestService.initAdalUserTable()
