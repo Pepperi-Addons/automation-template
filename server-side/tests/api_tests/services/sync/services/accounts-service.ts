ee54236dc356d85c3487beb3876946873f11fa50
Simcha Waintrob <simcha.w@pepperi.com>
2023-02-14 18:22:27 +0200

    getCurrentUserUUID

--- server-side/tests/api_tests/services/sync/services/accounts-service.ts
+++ server-side/tests/api_tests/services/sync/services/accounts-service.ts
@@ -1,13 +1,15 @@
 import { Client } from "@pepperi-addons/debug-server/dist";
 import { PapiClient } from "@pepperi-addons/papi-sdk";
 import { GlobalSyncService } from "./global-sync-service";
+import { UsersService } from "./users-service";
 export const CORE_RESOURCES_ADDON_UUID = 'fc5a5974-3b30-4430-8feb-7d5b9699bc9f'
 
 export class AccountsService {
     private papiClient: PapiClient;
-
+    private usersService: UsersService;
     constructor(papiClient: PapiClient) {        
         this.papiClient = papiClient;
+        this.usersService = new UsersService(this.papiClient);
     }
 
     
@@ -34,7 +36,7 @@ export class AccountsService {
     async getConnectedAccounts(): Promise<any[]> {
         // Get data
         const accounts = await this.papiClient.accounts.iter().toArray();
-        const currentUserUUID = GlobalSyncService.getCurrentUserUUID(this.papiClient);
+        const currentUserUUID = this.usersService.getCurrentUserUUID();
         const accountsUsers = await this.papiClient.get(`/addons/data/${CORE_RESOURCES_ADDON_UUID}/account_users?where=Hidden=0`);
 
         const connectedAccountUsers = accountsUsers.filter(accountUser => accountUser.User === currentUserUUID);
