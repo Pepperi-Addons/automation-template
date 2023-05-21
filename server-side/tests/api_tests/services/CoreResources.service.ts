8942ebf26eda082ed17fd55cbfa5d125df11b9d5
Saar Guttman <saar.g@pepperi.com>
2023-05-21 15:59:40 +0300

    Add Search + refactor cleanTable

--- server-side/tests/api_tests/services/CoreResources.service.ts
+++ server-side/tests/api_tests/services/CoreResources.service.ts
@@ -3,6 +3,9 @@ import {
     FindOptions,
     User,
     PapiClient,
+	SearchData,
+	AddonData,
+	SearchBody,
 } from '@pepperi-addons/papi-sdk';
 import GeneralService from 'test_infra';
 import { v4 as uuid } from 'uuid';
@@ -11,7 +14,7 @@ export class CoreResourcesService {
     papiClient: PapiClient;
     routerClient: PapiClient;
     generalService: GeneralService;
-    dataObject: any; // the 'Data' object passsed inside the http request sent to start the test -- put all the data you need here
+    dataObject: any; // the 'Data' object passed inside the http request sent to start the test -- put all the data you need here
 	addonUUID: string = 'fc5a5974-3b30-4430-8feb-7d5b9699bc9f';
 
    constructor(public systemService: GeneralService, public addonService: PapiClient, dataObject:any) {
@@ -22,7 +25,11 @@ export class CoreResourcesService {
     }
 
     async getGenericResourceObjects(resource: string): Promise<any[]> {
-        return this.papiClient.get(`/resources/${resource}`);
+        return await this.papiClient.get(`/resources/${resource}`);
+    }
+
+	async searchGenericResourceObjects(resource: string, searchBody: SearchBody): Promise<SearchData<AddonData>> {
+        return await this.papiClient.resources.resource(resource).search(searchBody);
     }
 
 	async getPapiResourceObjects(resource: string): Promise<any[]> {
@@ -87,9 +94,27 @@ export class CoreResourcesService {
 	}
 
 	async cleanTable(resource: string): Promise<void> {
-		let objects = await this.getGenericResourceObjects(resource);
-		objects.forEach(obj => obj.Hidden = true);
-		await this.papiClient.post(`/addons/data/batch/${this.addonUUID}/${resource}`, {Objects: objects});
+
+		let searchResponse: SearchData<AddonData>;
+		let NextPageKey: string | undefined = undefined;
+
+		do
+		{
+			const searchOptions: SearchBody = {
+				...(NextPageKey && {PageKey: NextPageKey}),
+				Fields: ["Key"]
+			};
+
+			searchResponse = await this.searchGenericResourceObjects(resource, searchOptions);
+			
+			// For each object, set the Hidden field to true
+			searchResponse.Objects.forEach(obj => obj.Hidden = true);
+
+			// Batch upsert to adal
+			this.papiClient.post(`/addons/data/batch/${this.addonUUID}/${resource}`, {Objects: searchResponse.Objects})
+			NextPageKey = searchResponse.NextPageKey;
+		}
+		while (NextPageKey);
 	}
 
 	async createContact(contact) {
