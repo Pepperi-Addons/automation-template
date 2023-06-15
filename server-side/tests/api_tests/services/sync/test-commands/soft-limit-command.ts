14f871a5c313968762e39d8f7ea273b3ab564386
Lior Sela <lior.s@pepperi.com>
2023-06-15 11:27:30 +0300

    few updates

--- server-side/tests/api_tests/services/sync/test-commands/soft-limit-command.ts
+++ server-side/tests/api_tests/services/sync/test-commands/soft-limit-command.ts
@@ -1,9 +1,8 @@
-import { AddonDataScheme } from "@pepperi-addons/papi-sdk";
 import { ADALTableService } from "../../resource_management/adal_table.service";
 import { GlobalSyncService } from "../services/global-sync-service";
-import { BaseCommand } from "./base-command";
+import { NumberOfRecordsCommand } from "./number-of-records-command";
 
-export class SoftLimitCommand extends BaseCommand {
+export class SoftLimitCommand extends NumberOfRecordsCommand {
 
     async setupSchemes(): Promise<ADALTableService> {
         const schema = this.syncAdalService.generateSchemeWithFields(1, `_${this.constructor.name}`)
@@ -11,9 +10,12 @@ export class SoftLimitCommand extends BaseCommand {
         return adalService
     }
     async pushData(adalService: ADALTableService): Promise<any> {
-        const data = this.syncAdalService.generateFieldsData(120,10000)
-        const size = Buffer.byteLength(JSON.stringify(data), 'utf8')/1024/1024
-        await adalService.upsertBatch(data)
+        const data = this.syncAdalService.generateFieldsData(1,100000,20)
+        console.log(`uploading data with size ${Buffer.byteLength(JSON.stringify(data), 'utf8')/1024/1024}MB`)
+        await this.syncDimxService.createRelation(this.resourceManager, adalService.schemaName)    
+
+        // upload data
+        await this.syncFileService.uploadFilesAndImport(data, adalService.schemaName)
 
         await this.syncService.setSyncSoftLimit(1)
 
@@ -28,14 +30,14 @@ export class SoftLimitCommand extends BaseCommand {
         return auditLog
     }
     async processSyncResponse(syncRes: any): Promise<any> {
-        this.syncDataResult.data =  await this.syncService.getSyncData(syncRes)
+        this.syncDataResult.data =  await this.syncService.extractSyncResData(syncRes)
         return this.syncDataResult.data;
     }
 
     async test(syncRes: any, syncData:any, expect: Chai.ExpectStatic): Promise<any> {
         // tests
-        expect(syncRes).to.have.property('UpToDate').that.is.a('Boolean').and.is.equal(false)
-        expect(syncRes).to.have.property('ExecutionURI').that.is.a('String').and.is.not.undefined
+        expect(syncData).to.have.property('success').that.is.a('Boolean').and.is.equal(false)
+        expect(syncData).to.have.nested.property('resultObject.errorMessage').that.is.a('String').and.is.include('The data size is too big. The maximum data size is 1 MB')
     }
 
     async cleanup(): Promise<any> {
